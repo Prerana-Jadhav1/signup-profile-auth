aa5ec549037ad8bda14e7af640602c55930c520f
Prerana-Jadhav1 <preranajb5@gmail.com>
2026-09-08 18:20:16 +0530

    Add files via upload

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,1 +1,32 @@
+import { SIGNUP_SUCCESS, LOGOUT } from "../actions/authActions";
 
+const getStoredUser = () => {
+  const storedUser = localStorage.getItem("user");
+  return storedUser ? JSON.parse(storedUser) : null;
+};
+
+const initialState = {
+  user: getStoredUser(),
+  accessToken: localStorage.getItem("accessToken") || null,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        user: action.payload.user,
+        accessToken: action.payload.accessToken,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        user: null,
+        accessToken: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
